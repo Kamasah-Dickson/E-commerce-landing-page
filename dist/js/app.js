8d681cc8a1fe57590ddac66aeadb5c7690f7e7f0
Kamasah Dickson <kamasahelizabeth15@gmail.com>
2022-11-13 07:51:12 +0000

    shoping cart UI update v2

--- dist/js/app.js
+++ dist/js/app.js
@@ -2,6 +2,23 @@ const menu = document.querySelector(".hamburger");
 const close_menu = document.querySelector(".close");
 const nav_lightbox = document.querySelector(".lightbox");
 const mobile_navigation = document.querySelector(".mobile-navigation");
+const nav = document.querySelector(".nav-links");
+const nav_links = nav.querySelectorAll("li");
+const header = document.querySelector("header");
+const sneakers = document.querySelectorAll(".thumb");
+
+window.addEventListener("scroll", scrolling);
+
+function scrolling() {
+	if (
+		document.body.scrollTop >= 35 ||
+		document.documentElement.scrollTop >= 35
+	) {
+		header.classList.add("active-scroll");
+	} else {
+		header.classList.remove("active-scroll");
+	}
+}
 
 menu.addEventListener("click", () => {
 	mobile_navigation.classList.add("show-nav");
@@ -15,4 +32,22 @@ close_menu.addEventListener("click", () => {
 nav_lightbox.addEventListener("click", () => {
 	mobile_navigation.classList.remove("show-nav");
 	nav_lightbox.classList.remove("show-lightbox");
+});
+
+nav_links.forEach((nav, index) => {
+	nav.addEventListener("click", () => {
+		nav_links.forEach((nav) => {
+			nav.classList.remove("active");
+		});
+		nav_links[index].classList.add("active");
+	});
+});
+
+sneakers.forEach((sneaker, index) => {
+	sneaker.addEventListener("click", () => {
+		sneakers.forEach((sneaker) => {
+			sneaker.classList.remove("active");
+		});
+		sneakers[index].classList.add("active");
+	});
 });
