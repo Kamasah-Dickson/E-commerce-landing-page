242d37b074eec42ba381427ae752e4f7f854f7ce
Kamasah Dickson <kamasahelizabeth15@gmail.com>
2022-11-14 10:47:49 +0000

    checkout UI completed

--- dist/js/app.js
+++ dist/js/app.js
@@ -5,14 +5,100 @@ const mobile_navigation = document.querySelector(".mobile-navigation");
 const nav = document.querySelector(".nav-links");
 const nav_links = nav.querySelectorAll("li");
 const header = document.querySelector("header");
+const sneaker_container = document.querySelector(".review-sneakers");
+const main_review = document.querySelector(".showcase img");
+const trackData = JSON.parse(localStorage.getItem("data")) || [];
+
+window.addEventListener("DOMContentLoaded", () => {
+	shoeReview();
+});
+
+const showcase = [
+	{
+		id: 1,
+		img: "./images/image-product-1.jpg",
+	},
+	{
+		id: 1,
+		img: "./images/image-product-2.jpg",
+	},
+	{
+		id: 1,
+		img: "./images/image-product-3.jpg",
+	},
+	{
+		id: 1,
+		img: "./images/image-product-4.jpg",
+	},
+];
+
+sneaker_container.addEventListener("click", (e) => {
+	let img = e.target.src;
+	let ImgID = parseInt(e.target.id);
+	main_review.src = img;
+	img = img.slice(21);
+
+	// console.log(img);
+
+	let results = trackData.find((data) => {
+		return data.id === ImgID;
+	});
+	if (results === undefined) {
+		trackData.push({
+			id: ImgID,
+			img: img,
+		});
+	} else {
+		results.id = ImgID;
+		results.img = img;
+	}
+	localStorage.setItem("data", JSON.stringify(trackData));
+});
+
+function shoeReview() {
+	main_review.src = `.${trackData[0].img}`;
+
+	data = localStorage.getItem("active");
+	sneakers[data].classList.add("active");
+	localStorage.setItem("active", data);
+}
+
+function generateShoe() {
+	return (sneaker_container.innerHTML = showcase
+		.map((shoe, index) => {
+			let { id, img } = shoe;
+			return `
+			<div class="thumb" id ="${id}" data-index ="${index}">
+			<img
+			class="sneaker" id ="${id}"
+			src="${img}"
+			alt="sneakers"/>
+			</div>`;
+		})
+		.join(""));
+}
+generateShoe();
+
 const sneakers = document.querySelectorAll(".thumb");
+
+sneakers.forEach((sneaker) => {
+	sneaker.addEventListener("click", () => {
+		data = sneaker.dataset.index;
+
+		sneakers.forEach((sneaker) => {
+			sneaker.classList.remove("active");
+		});
+		sneakers[data].classList.add("active");
+		localStorage.setItem("active", data);
+	});
+});
 
 window.addEventListener("scroll", scrolling);
 
 function scrolling() {
 	if (
-		document.body.scrollTop >= 70 ||
-		document.documentElement.scrollTop >= 70
+		document.body.scrollTop >= 80 ||
+		document.documentElement.scrollTop >= 80
 	) {
 		header.classList.add("active-scroll");
 	} else {
@@ -42,14 +128,5 @@ nav_links.forEach((nav, index) => {
 			nav.classList.remove("active");
 		});
 		nav_links[index].classList.add("active");
-	});
-});
-
-sneakers.forEach((sneaker, index) => {
-	sneaker.addEventListener("click", () => {
-		sneakers.forEach((sneaker) => {
-			sneaker.classList.remove("active");
-		});
-		sneakers[index].classList.add("active");
 	});
 });
