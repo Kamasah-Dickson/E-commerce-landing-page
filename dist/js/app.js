375be9b1a92ea66952c3c4513ea8d1178aaceebc
Kamasah Dickson <kamasahelizabeth15@gmail.com>
2022-11-17 16:23:31 +0000

    img paths updated

--- dist/js/app.js
+++ dist/js/app.js
@@ -24,19 +24,19 @@ window.addEventListener("DOMContentLoaded", () => {
 const showcase = [
 	{
 		id: 1,
-		img: "./images/image-product-1.jpg",
+		img: "images/image-product-1.jpg",
 	},
 	{
 		id: 1,
-		img: "./images/image-product-2.jpg",
+		img: "images/image-product-2.jpg",
 	},
 	{
 		id: 1,
-		img: "./images/image-product-3.jpg",
+		img: "images/image-product-3.jpg",
 	},
 	{
 		id: 1,
-		img: "./images/image-product-4.jpg",
+		img: "images/image-product-4.jpg",
 	},
 ];
 
